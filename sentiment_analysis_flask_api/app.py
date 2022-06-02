a42b0c7b74aa44f539c18c5f4cbf682dfcbadf66
Rajat Gupta <rajatgba2021@email.iimcal.ac.in>
2022-06-02 09:38:59 +0530

    Add files via upload

--- sentiment_analysis_flask_api/app.py
+++ sentiment_analysis_flask_api/app.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 from flask import Flask, request, jsonify, render_template
 
 # initialize sentiment model
-classifier = pipeline('sentiment-analysis')
+classifier = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
 
 # get sentiment
 def sentiment_prediction(sentence):
@@ -11,16 +11,13 @@ def sentiment_prediction(sentence):
 # initialize Flask app
 app = Flask(__name__)
 
-# creating an endpoint
+# creating endpoints
 @app.route('/')
-def home():
+def home(): 
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''    
     sentence = str(request.form['sentence'])
     response = sentiment_prediction(sentence)[0]
     label = response['label']
@@ -28,4 +25,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Sentiment predicted is {label} with probability {score}')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0', port = 3000)
